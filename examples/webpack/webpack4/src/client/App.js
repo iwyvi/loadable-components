02ab0537e540b30f98c9aa75ae8b9d34e201cd08
iwyvi <iwyvi@outlook.com>
2020-10-21 17:37:56 +0800

    chore: modify example (#634)

--- examples/webpack/webpack4/src/client/App.js
+++ examples/webpack/webpack4/src/client/App.js
@@ -3,19 +3,19 @@ import loadable from '@loadable/component'
 
 const X = loadable(props => import(`./letters/${props.letter}`))
 
-const Moment = loadable.lib(() => import('moment'), {
-  resolveComponent: moment => moment.default || moment,
-})
+// const Moment = loadable.lib(() => import('moment'), {
+//   resolveComponent: moment => moment.default || moment,
+// })
 
 const App = () => (
   <div>
     <p>
     Lazy load letter A:<X letter="A" />
     </p>
-    <p>
+    {/* <p>
       lazy load momentjs:
       <Moment>{moment => `now is : ${moment().format('HH:mm')}`}</Moment>
-    </p>
+    </p> */}
   </div>
 )
 
